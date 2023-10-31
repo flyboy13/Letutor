5ec90b411bfc8d005b74ceef765dfcbd9a6db78a
Nguyen Duc Tai <20120568@student.hcmus.edu.vn>
2023-10-31 14:59:41 +0700

    Add login screen

--- letutor/lib/main.dart
+++ letutor/lib/main.dart
@@ -1,113 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:letutor/login_screen.dart';
+import 'package:letutor/teacher_screen.dart';
+import 'package:letutor/test.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter UI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter UI'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title});
-  final String title;
+  const MyApp({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Center(
-                child: Image.network(
-                  'https://i.imgur.com/KrCxQTg.png',
-                  height: 150,
-                  width: 150,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Say hello to your English tutors',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Become fluent faster through one on one video chat lessons tailored to your goals.',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your email',
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // TODO: Handle button press
-                },
-                child: Text('Subscribe'),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Text(
-                  'OR',
-                  style: TextStyle(fontSize: 18, color: Colors.grey),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // TODO: Handle button press
-                  },
-                  child: Text('Continue with Google'),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // TODO: Handle button press
-                  },
-                  child: Text('Continue with Facebook'),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // TODO: Handle button press
-                  },
-                  child: Text('Continue with Apple'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  Widget build(context) {
+    return MaterialApp(home: TeacherPage());
   }
 }
